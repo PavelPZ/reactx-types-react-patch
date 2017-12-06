76e81b87aef86505fa6a9e2bcaa5e50bf9adb660
Pavel Zika <pzika@langmaster.cz>
2017-12-06 12:12:05 +0100

    v

--- publish.cmd
+++
@@ -1,9 +0,0 @@
-d:
-
-cd D:\rw\pavelpz\reactx-types-react-patch\src
-call git archive -o reactx-types-react-patch.tar
-call npm version patch
-call npm publish
-
-
-call git archive --format tar -o reactx-types-react-patch.tar.tz HEAD:src
